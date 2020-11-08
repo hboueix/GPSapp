e6b5a7e4e54c2f137e40106412c48c5b450a1d78
vivimouret29 <vivien.mouret@ynov.com>
2020-11-08 19:57:17 +0100

    🚀 deploying profile page with input functions

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,22 +1,114 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import './Profile.css';
+import {
+  IonPage,
+  IonHeader,
+  IonButtons,
+  IonMenuButton,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonContent,
+  IonItem,
+  IonItemDivider,
+  IonList,
+  IonAlert,
+  IonGrid,
+  IonRow,
+  IonLabel,
+  IonInput
+} from '@ionic/react';
+
+import React, { useState, useContext } from 'react';
+// import React, { useState } from 'react';
+
+// import FinancialInfoItem from '../components/FinancialInfoItem';
+import AppContext from '../data/app-context';
+import ResponsiveContent from '../components/ResponsiveContent';
 
 const Profile: React.FC = () => {
+  const [showAlert, setShowAlert] = useState(false);
+  const [latitude] = useState<number>(-63572375290155);
+  const [longitude] = useState<number>(106744840359415);
+
+  // const updateUsername = (event: CustomEvent) => {
+  //   setName(event.detail.value!);
+  // }
+
+  const appCtx = useContext(AppContext)
+
+  const updateUsername = (newUsername: string) => {
+    let updatedProfile = { ...appCtx.profile }
+    updatedProfile.username = newUsername;
+    appCtx.updateProfile(updatedProfile);
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Profile</IonTitle>
+          <IonButtons slot="start">
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Profil de l'utilisateur</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Profile</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+        <IonGrid>
+          <IonRow>
+            <ResponsiveContent>
+              <IonList className="ion-padding" mode="md">
+                <IonCard>
+                  <IonItemDivider>Name</IonItemDivider>
+                  <IonItem>
+                    <IonInput onClick={() => setShowAlert(true)}>{appCtx.profile.username}</IonInput>
+                  </IonItem>
+                </IonCard>
+                <IonCard >
+                  <IonItemDivider>Last Latitude</IonItemDivider>
+                  <IonItem>
+                    <IonLabel>{latitude}</IonLabel>
+                  </IonItem>
+                </IonCard>
+                <IonCard >
+                  <IonItemDivider>Last Longitude</IonItemDivider>
+                  <IonItem>
+                    <IonLabel>{longitude}</IonLabel>
+                  </IonItem>
+                </IonCard>
+              </IonList>
+            </ResponsiveContent>
+          </IonRow>
+        </IonGrid>
       </IonContent>
+      <IonAlert
+        isOpen={showAlert}
+        onDidDismiss={() => setShowAlert(false)}
+        header={'Username'}
+        inputs={[
+          {
+            name: 'usernameInput',
+            type: 'text',
+            id: 'profile-username',
+            value: appCtx.profile.username,
+            placeholder: 'Your username'
+          }
+        ]}
+        buttons={[
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            handler: () => {
+              console.log('Confirm Cancel');
+            }
+          },
+          {
+            text: 'Ok',
+            handler: (changeInput) => {
+              updateUsername(changeInput.usernameInput)
+              console.log('Confirm Ok')
+            }
+          }
+        ]}
+      />
     </IonPage>
   );
 };
