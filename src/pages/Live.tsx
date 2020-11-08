b18e50cd46c91f8f5788a72d04d36420a505c2a2
Hugo Boueix <hugo.boueix@outlook.com>
2020-11-08 19:21:28 +0100

    :sparkles: mockup done

--- src/pages/Live.tsx
+++ src/pages/Live.tsx
@@ -1,21 +1,39 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonGrid, IonHeader, IonIcon, IonItem, IonLabel, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { save } from 'ionicons/icons';
 import React from 'react';
-import './Live.css';
+import ResponsiveContent from '../components/ResponsiveContent';
+import './Live.scss';
 
 const Live: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Live</IonTitle>
+    <IonPage id="Live">
+      <IonHeader id="headerRow">
+        <IonToolbar className='ion-text-center'>
+          <IonTitle>Live position</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Live</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+      <IonContent className='ion-padding'>
+        <IonGrid>
+          <IonRow className='ion-text-center'>
+            <ResponsiveContent>
+              <IonLabel>Your current position is :</IonLabel>
+            </ResponsiveContent>
+          </IonRow>
+          <IonRow>
+            <ResponsiveContent>
+              <IonLabel>Latitude : 45.1234567890</IonLabel>
+            </ResponsiveContent>
+          </IonRow>
+          <IonRow>
+            <ResponsiveContent>
+              <IonLabel>Longitude : 5.0987654321</IonLabel>
+            </ResponsiveContent>
+          </IonRow>
+        </IonGrid>
+        <IonButton className="ion-float-right">
+          <IonIcon icon={save}></IonIcon>
+          <IonLabel>Save !</IonLabel>
+        </IonButton>
       </IonContent>
     </IonPage>
   );
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,21 +1,37 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonGrid, IonHeader, IonLabel, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
-import './Profile.css';
+import ResponsiveContent from '../components/ResponsiveContent';
+import './Profile.scss';
 
 const Profile: React.FC = () => {
   return (
-    <IonPage>
+    <IonPage id="Profile">
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Profile</IonTitle>
+          <IonTitle className='ion-text-center'>Profile</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Profile</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+      <IonContent>
+        <IonGrid className='ion-padding'>
+          <IonRow>
+            <ResponsiveContent>
+              <IonLabel>Username</IonLabel>
+              <IonLabel className='ion-float-right'>hboueix</IonLabel>
+            </ResponsiveContent>
+          </IonRow>
+          <IonRow>
+            <ResponsiveContent>
+              <IonLabel>Last latitude</IonLabel>
+              <IonLabel className='ion-float-right'>45.1234567890</IonLabel>
+            </ResponsiveContent>
+          </IonRow>
+          <IonRow>
+            <ResponsiveContent>
+              <IonLabel>Last longitude</IonLabel>
+              <IonLabel className='ion-float-right'>5.0987654321</IonLabel>
+            </ResponsiveContent>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
