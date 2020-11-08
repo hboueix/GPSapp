34eb5a8f1f8be373050a6c1264cca996575afb86
njkf <jules.vendee@ynov.com>
2020-11-08 22:31:22 +0100

    geolocation

--- src/pages/Live.tsx
+++ src/pages/Live.tsx
@@ -1,8 +1,20 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonButton, IonCard, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState} from "react"
+import { Geolocation, Geoposition } from "@ionic-native/geolocation"
 import './Live.css';
 
 const Live: React.FC = () => {
+    const [position, setPosition] = useState<Geoposition>();
+    
+    const getLocation = async() => {
+        const position = await Geolocation.getCurrentPosition();
+        setPosition(position);
+    }
+    getLocation();
+    
+    let latitude = position && position.coords.latitude
+    let longitude =  position && position.coords.longitude
+      
   return (
     <IonPage>
       <IonHeader>
@@ -13,12 +25,15 @@ const Live: React.FC = () => {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Live</IonTitle>
+            <IonTitle size="large">Live position</IonTitle>
           </IonToolbar>
         </IonHeader>
+              Latitude:{latitude}
+              Longitude:{longitude}
       </IonContent>
     </IonPage>
   );
 };
 
 export default Live;
+
